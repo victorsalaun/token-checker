89740adeb3fb0792897084494d67c6ee4bb488e7
Victor SALAUN <vsalaun@talend.com>
2019-09-03 20:04:29 +0200

    use env variables for aws secrets

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/victorsalaun/token-checker
 
 go 1.12
 
-require github.com/aws/aws-sdk-go v1.23.13
+require (
+	github.com/aws/aws-sdk-go v1.23.13
+	github.com/kelseyhightower/envconfig v1.4.0
+)
--- main.go
+++ main.go
@@ -3,21 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+	"os"
 )
 
-var sess = session.Must(session.NewSession())
+var creds = &aws.Config{}
 var region = &aws.Config{Region: aws.String("us-east-1")}
+var sess = session.Must(session.NewSession())
+
+type Token struct {
+	AwsAccessKeyId     string `envconfig:"AWS_ACCESS_KEY_ID" required:"true"`
+	AwsSecretAccessKey string `envconfig:"AWS_SECRET_ACCESS_KEY" required:"true"`
+}
 
 func main() {
+	os.Exit(process(os.Args[1:]))
+}
+
+func process(args []string) int {
+	var token Token
+	if err := envconfig.Process("", &token); err != nil {
+		log.Printf("[ERROR] Failed to process env var: %s", err)
+		return 1
+	}
+
+	creds = &aws.Config{Credentials: credentials.NewStaticCredentials(token.AwsAccessKeyId, token.AwsSecretAccessKey, "")}
+
 	iamChecker()
 	s3Checker()
+
+	return 0
 }
 
 func iamChecker() {
-	iamsvc := iam.New(sess, region)
+	iamsvc := iam.New(sess, creds, region)
 
 	result, err := iamsvc.GetUser(&iam.GetUserInput{})
 
@@ -30,7 +54,7 @@ func iamChecker() {
 }
 
 func s3Checker() {
-	s3svc := s3.New(sess, region)
+	s3svc := s3.New(sess, creds, region)
 
 	result, err := s3svc.ListBuckets(&s3.ListBucketsInput{})
 
